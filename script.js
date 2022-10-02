2f43b5c66deb910b0a5ea58a8f0c75936027efd1
EltonLunardi <eltonlunardi00@gmail.com>
2022-10-02 16:42:00 -0300

    commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-	$(".dvHex").hover(function(){
-		$(this).children(".hexTop").css("border-bottom-color","#ffa605");
-		$(this).children(".hexCenter").css("background-color","#ffa605");
-		$(this).children(".hexBottom").css("border-top-color","#ffa605");
-	}, function() {
-		$(".hexTop").css("border-bottom-color","#1e2530");
-		$(".hexCenter").css("background-color","#1e2530");
-		$(".hexBottom").css("border-top-color","#1e2530");
+$(document).ready(function () {
+	$(".dvHex").hover(function () {
+		$(this).children(".hexTop").css("border-bottom-color", "#ffa605");
+		$(this).children(".hexCenter").css("background-color", "#ffa605");
+		$(this).children(".hexBottom").css("border-top-color", "#ffa605");
+	}, function () {
+		$(".hexTop").css("border-bottom-color", "#1e2530");
+		$(".hexCenter").css("background-color", "#1e2530");
+		$(".hexBottom").css("border-top-color", "#1e2530");
 	});
 });
